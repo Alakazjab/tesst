bd38125c82f3ace04a4b0d03ae40b3b16d17eef2
aboba <ss@ss.com>
2022-10-31 18:36:16 +0300

    first commit

--- pr4.py
+++ pr4.py
@@ -1,77 +1,79 @@
-
-
-# a={"2","3"}
-# b={"4","3"}
-# c=a|b
-# print(c)
-#
-# a = {'a','s','d'}
-# x =input()
-# b=a
-# b.add(x)
-# print(b)
 #
 #
-# a = """
-# 3\n
-# asdasd asd ad a da dadsda\n
-# gdfgsdgf dfgdfg dfgdfg\n
-# dfgdgdsfa sdfsdfsdf\n
-# """
-# a=a.split()
-# print(set(a))
-
-
+# # a={"2","3"}
+# # b={"4","3"}
+# # c=a|b
+# # print(c)
+# #
+# # a = {'a','s','d'}
+# # x =input()
+# # b=a
+# # b.add(x)
+# # print(b)
+# #
+# #
+# # a = """
+# # 3\n
+# # asdasd asd ad a da dadsda\n
+# # gdfgsdgf dfgdfg dfgdfg\n
+# # dfgdgdsfa sdfsdfsdf\n
+# # """
+# # a=a.split()
+# # print(set(a))
 #
-# try:
-    # x1 = input()
-#     x2 = input()
-#     result=int(x1)*int(x2)
-# except ValueError:
-#     result=x1+x2
-# finally:
-#     print(result)
 #
+# #
+# # try:
+#     # x1 = input()
+# #     x2 = input()
+# #     result=int(x1)*int(x2)
+# # except ValueError:
+# #     result=x1+x2
+# # finally:
+# #     print(result)
+# #
+# #
+# # try:
+# #     t = 0
+# #     while True:
+# #
+# #         per = int(input())
+# #         t+=per
+# # except KeyboardInterrupt:
+# #     print(per)
 #
-# try:
-#     t = 0
-#     while True:
-#
-#         per = int(input())
-#         t+=per
-# except KeyboardInterrupt:
-#     print(per)
-
-# F = open('text.txt','w')
-# per = input()
-# F.writelines(per+'\n')
-# while per !='':
-#     per = input()
-#     F.writelines(per+'\n')
-# F.close()
-# from random import randrange
-# f = open('text.txt','w')
-# for i in range(10):
-#     f.writelines(str(randrange(-2147483647,2147483647))+'\n')
-# f.close()
-# f = open('text.txt')
-# result = 0
-# for i in range(10):
-#     x=int(f.readline())
-#     if x%2==0:
-#         result+=x
-    else if
-# print(result)
-from math import sqrt
+# # F = open('text.txt','w')
+# # per = input()
+# # F.writelines(per+'\n')
+# # while per !='':
+# #     per = input()
+# #     F.writelines(per+'\n')
+# # F.close()
+# # from random import randrange
+# # f = open('text.txt','w')
+# # for i in range(10):
+# #     f.writelines(str(randrange(-2147483647,2147483647))+'\n')
+# # f.close()
+# # f = open('text.txt')
+# # result = 0
+# # for i in range(10):
+# #     x=int(f.readline())
+# #     if x%2==0:
+# #         result+=x
+#     else if
+# # print(result)
+# from math import sqrt
 # f = open('text.txt')
 # b=int(f.readline())
 # f.close()
-# f = open('text.txt','a')
-# z1=b
-# z2=b*2
-# f.writelines(str(z1)+'\n')
-# f.writelines(str(z2)+'\n')
-# f.close()
-x=10
-while x != 0:
-    x=x-1
+def maximum(mas):
+    return mas.index(max(mas))
+f = open('text.txt','r')
+n=f.readline()
+mas =[]
+while n!='':
+    mas.append(float(n))
+    n = f.readline()
+print(str(min(mas)+max(mas)))
+f.close()
+print(maximum(mas))
